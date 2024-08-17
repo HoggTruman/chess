d0f43901afd827162047a0d95c8928e6045fdce4
Truman Hogg <hoggtruman@gmail.com>
2024-08-17 22:18:58 +0100

    Add OppositeColor helper

---
+++ GameLogic/Helpers/ColorHelpers.cs
@@ -0,0 +1,19 @@
+﻿using GameLogic.Enums;
+
+namespace GameLogic.Helpers;
+
+public static class ColorHelpers
+{
+    /// <summary>
+    /// Returns Black if White and White if Black
+    /// </summary>
+    /// <param name="color"></param>
+    /// <returns>A Color Enum value</returns>
+    public static Color OppositeColor(Color color)
+    {
+        if (color == Color.White)
+            return Color.Black;
+        else
+            return Color.White;
+    }
+}
