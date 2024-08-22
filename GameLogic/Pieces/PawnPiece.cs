91e4df354c1805ba3fbfa29f47c06b1d9467facd
Truman Hogg <hoggtruman@gmail.com>
2024-08-22 22:12:59 +0100

    Implement Pawn GetReachableSquares method

--- GameLogic/Pieces/PawnPiece.cs
+++ GameLogic/Pieces/PawnPiece.cs
@@ -1,6 +1,7 @@
 using GameLogic.Constants;
 using GameLogic.Enums;
 using GameLogic.Helpers;
+using GameLogic.Interfaces;
 
 namespace GameLogic.Pieces;
 
@@ -51,11 +52,84 @@ public class PawnPiece : Piece
         return targetedSquares;
     }
 
+
     public override List<(int row, int col)> GetReachableSquares(Board board)
     {
-        throw new NotImplementedException();
+        List<(int row, int col)> squares = [];
 
-        // en passant requires move history to check that the opponent's pawn moved into position last turn
+        // Add non-capturing move squares
+        if (Color == Color.White)
+        {
+            if (Row + 1 <= Board.MaxIndex && board.State[Row + 1, Col] == null)
+            {
+                squares.Add(new(Row + 1, Col));
+
+                if (HasMoved(board) == false && board.State[Row + 2, Col] == null)
+                {
+                    squares.Add(new(Row + 2, Col));
+                }
+            }
+        }
+        else if (Color == Color.Black)
+        {
+            if (Row - 1 >= Board.MinIndex && board.State[Row - 1, Col] == null)
+            {
+                squares.Add(new(Row - 1, Col));
+
+                if (HasMoved(board) == false && board.State[Row - 2, Col] == null)
+                {
+                    squares.Add(new(Row - 2, Col));
+                }
+            }
+        }
+
+        // Add standard capturing moves (not En Passant)
+        if (Color == Color.White)
+        {
+            if (Col - 1 >= Board.MinIndex && Row + 1 <= Board.MaxIndex && board.State[Row + 1, Col - 1]?.Color == Color.Black)
+                squares.Add(new(Row + 1, Col - 1));
+            
+            if (Col + 1 <= Board.MaxIndex && Row + 1 <= Board.MaxIndex && board.State[Row + 1, Col + 1]?.Color == Color.Black)
+                squares.Add(new(Row + 1, Col + 1));
+        }
+        else if (Color == Color.Black)
+        {
+            if (Col - 1 >= Board.MinIndex && Row - 1 >= Board.MinIndex && board.State[Row - 1, Col - 1]?.Color == Color.Black)
+                squares.Add(new(Row - 1, Col - 1));
+            
+            if (Col + 1 <= Board.MaxIndex && Row - 1 >= Board.MinIndex && board.State[Row - 1, Col + 1]?.Color == Color.Black)
+                squares.Add(new(Row - 1, Col + 1));
+        }
+
+        // Add En Passant square if there is one
+        var enPassantSquare = GetEnPassantSquare(board);
+        if (enPassantSquare != null)
+            squares.Add(((int row, int col))enPassantSquare);
+
+        
+        return squares;
     }
+
+    private (int row, int col)? GetEnPassantSquare(Board board)
+    {
+        IMove? lastMove = board.MoveHistory.LastOrDefault();
+
+        if (
+            lastMove != null &&
+            lastMove.MovingPiece.PieceType == PieceType.Pawn &&
+            Math.Abs(lastMove.From.row - lastMove.To.row) == 2 &&
+            lastMove.To.row == Row 
+        )
+        {
+            if (Col == lastMove.To.col - 1 || Col == lastMove.To.col + 1)
+            {
+                return ((lastMove.From.row + lastMove.To.row) / 2, lastMove.To.col);
+            }
+        }
+
+        return null;
+    }
+
+
 
 }
