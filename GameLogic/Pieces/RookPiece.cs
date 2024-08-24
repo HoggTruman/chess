0a34fb2b14acc27549265e29f105b9a2873a5901
Truman Hogg <hoggtruman@gmail.com>
2024-08-24 23:42:42 +0100

    Add CanCastle tests

--- GameLogic/Pieces/RookPiece.cs
+++ GameLogic/Pieces/RookPiece.cs
@@ -24,9 +24,60 @@ public class RookPiece : Piece
         return PieceHelpers.ScanRowAndCol(Row, Col, board);
     }
 
+
     public override List<(int row, int col)> GetReachableSquares(Board board)
     {
         throw new NotImplementedException();
+    }
+
+
+    /// <summary>
+    /// Returns true if all conditions are met for castling with this piece.
+    /// </summary>
+    /// <param name="board"></param>
+    /// <returns></returns>
+    public bool CanCastle(Board board)
+    {
+        // Ensure there is a king to castle with
+        var king = board.Kings[Color];
+
+        if (king == null)
+            return false;
+
+        // Ensure the rook and king have not moved
+        if (HasMoved(board) || king.HasMoved(board))
+            return false;
+
+        // Ensure there are no pieces between the two
+        List<(int row, int col)> betweenSquares = [];
+
+        if (Col < king.Col)
+            betweenSquares = [(Row, Col + 1), (Row, Col + 2), (Row, Col + 3)];
+        
+        if (Col > king.Col)
+            betweenSquares = [(Row, Col - 1), (Row, Col - 2)];
+
+        if (betweenSquares.Any(x => board.State[x.row, x.col] != null))
+            return false;
+
+        // Ensure the king does not pass through check
+        List<(int row, int col)> kingSquares = [];
+
+        if (Col < king.Col)
+            kingSquares = [king.Square, (king.Row, king.Col - 1), (king.Row, king.Col - 2)];
+
+        if (Col > king.Col)
+            kingSquares = [king.Square, (king.Row, king.Col + 1), (king.Row, king.Col + 2)];
+        
+        var enemyPieces = board.GetPiecesByColor(ColorHelpers.OppositeColor(Color));
+
+        foreach (var piece in enemyPieces)
+        {
+            if (piece.GetTargetedSquares(board).Intersect(kingSquares).Any())
+                return false;
+        }
+        
+        return true;
     }
 
 }
---
+++ GameLogicTests/Pieces/RookPieceTests.cs
@@ -0,0 +1,163 @@
+using FluentAssertions;
+using GameLogic;
+using GameLogic.Constants;
+using GameLogic.Enums;
+using GameLogic.Interfaces;
+using GameLogic.Moves;
+using GameLogic.Pieces;
+
+namespace GameLogicTests.Pieces;
+
+public class RookPieceTests
+{
+    #region CanCastle Tests 
+
+    [Fact]
+    public void CanCastle_WhiteKingSideRook_ReturnsTrue()
+    {
+        // Arrange
+        Board testBoard = new();
+
+        KingPiece king = new(StartSquares.WhiteKing.row, StartSquares.WhiteKing.col);
+        RookPiece rook = new(StartSquares.WhiteRookK.row, StartSquares.WhiteRookK.col);
+
+        List<IPiece> pieces = [king, rook];
+
+        testBoard.AddPieces(pieces);
+
+        // Act
+        var result = rook.CanCastle(testBoard);
+
+        // Assert 
+        result.Should().BeTrue();
+    }
+
+
+    [Theory]
+    [InlineData(5)] // bishop col
+    [InlineData(6)] // knight col
+    public void CanCastle_WhiteKingSideWhenBlocked_ReturnsFalse(int blockedCol)
+    {
+        // Arrange
+        Board testBoard = new();
+
+        KingPiece king = new(StartSquares.WhiteKing.row, StartSquares.WhiteKing.col);
+        RookPiece rook = new(StartSquares.WhiteRookK.row, StartSquares.WhiteRookK.col);
+
+        QueenPiece blockingPiece = new(StartSquares.WhiteKing.row, blockedCol);
+
+        List<IPiece> pieces = [king, rook, blockingPiece];
+
+        testBoard.AddPieces(pieces);
+
+        // Act
+        var result = rook.CanCastle(testBoard);
+
+        // Assert 
+        result.Should().BeFalse();
+    }
+
+
+    [Theory]
+    [InlineData(4)] // king col
+    [InlineData(5)] // bishop col
+    [InlineData(6)] // knight col
+    public void CanCastle_WhiteKingSideWhenKingTargeted_ReturnsFalse(int targetedCol)
+    {
+        // Tests that CanCastle returns false when each of the squares the king passes
+        // through (including its starting square) are targeted by an enemy piece
+
+        // Arrange
+        Board testBoard = new();
+
+        KingPiece king = new(StartSquares.WhiteKing.row, StartSquares.WhiteKing.col, Color.White);
+        RookPiece rook = new(StartSquares.WhiteRookK.row, StartSquares.WhiteRookK.col, Color.White);
+
+        RookPiece enemyPiece = new(StartSquares.BlackKing.row, targetedCol, Color.Black);
+
+        List<IPiece> pieces = [king, rook, enemyPiece];
+
+        testBoard.AddPieces(pieces);
+
+        // Act
+        var result = rook.CanCastle(testBoard);
+
+        // Assert 
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void CanCastle_WhiteKingSideWhenOnlyRookTargeted_ReturnsTrue()
+    {
+        // Tests that CanCastle returns true when each of the squares that only the rook
+        // passes through are targeted by an enemy piece
+
+        // Arrange
+        Board testBoard = new();
+
+        KingPiece king = new(StartSquares.WhiteKing.row, StartSquares.WhiteKing.col, Color.White);
+        RookPiece rook = new(StartSquares.WhiteRookK.row, StartSquares.WhiteRookK.col, Color.White);
+
+        RookPiece enemyPiece = new(StartSquares.BlackKing.row, StartSquares.WhiteRookK.col, Color.Black);
+
+        List<IPiece> pieces = [king, rook, enemyPiece];
+
+        testBoard.AddPieces(pieces);
+
+        // Act
+        var result = rook.CanCastle(testBoard);
+
+        // Assert 
+        result.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public void CanCastle_WhiteKingSideWhenKingHasMoved_ReturnsFalse()
+    {
+        // Arrange
+        Board testBoard = new();
+
+        KingPiece king = new(StartSquares.WhiteKing.row + 1, StartSquares.WhiteKing.col);
+        RookPiece rook = new(StartSquares.WhiteRookK.row, StartSquares.WhiteRookK.col);
+
+        List<IPiece> pieces = [king, rook];
+
+        testBoard.AddPieces(pieces);
+
+        Move kingMove = new(MoveType.Move, king.Square, StartSquares.WhiteKing, king);
+        testBoard.HandleMove(kingMove);
+
+        // Act
+        var result = rook.CanCastle(testBoard);
+
+        // Assert 
+        result.Should().BeFalse();
+    }
+
+
+    [Fact]
+    public void CanCastle_WhiteKingSideWhenRookHasMoved_ReturnsFalse()
+    {
+        // Arrange
+        Board testBoard = new();
+
+        KingPiece king = new(StartSquares.WhiteKing.row, StartSquares.WhiteKing.col);
+        RookPiece rook = new(StartSquares.WhiteRookK.row + 1, StartSquares.WhiteRookK.col);
+
+        List<IPiece> pieces = [king, rook];
+
+        testBoard.AddPieces(pieces);
+
+        Move rookMove = new(MoveType.Move, rook.Square, StartSquares.WhiteRookK, rook);
+        testBoard.HandleMove(rookMove);
+
+        // Act
+        var result = rook.CanCastle(testBoard);
+
+        // Assert 
+        result.Should().BeFalse();
+    }
+
+    #endregion
+}
