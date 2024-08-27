127e790afb58a024165ca80eb08235169eec6ad0
Truman Hogg <hoggtruman@gmail.com>
2024-08-27 01:16:18 +0100

    Add GetValidMoves override on KingPiece

--- GameLogic/Pieces/KingPiece.cs
+++ GameLogic/Pieces/KingPiece.cs
@@ -1,6 +1,8 @@
 using GameLogic.Constants;
 using GameLogic.Enums;
 using GameLogic.Helpers;
+using GameLogic.Interfaces;
+using GameLogic.Moves;
 
 namespace GameLogic.Pieces;
 
@@ -49,6 +51,60 @@ public class KingPiece : Piece
             .ToList();
 
         return squares;
+    }
+
+
+    /// <summary>
+    /// Returns a List of Moves including CastleMoves where possible.
+    /// </summary>
+    /// <param name="board"></param>
+    /// <returns></returns>
+    public override List<IMove> GetValidMoves(Board board)
+    {
+        // Get all valid standard moves
+        var validMoves = base.GetValidMoves(board);
+
+        // Add valid castling moves
+        var rooks = (List<RookPiece>)board.GetPiecesByColor(Color)
+            .Where(x => x is RookPiece);
+
+        foreach (var rook in rooks)
+        {
+            var castleSquares = GetCastleSquares(board, rook);
+
+            if (castleSquares != null)
+            {
+                var kingTo = castleSquares.Value.kingTo;
+                var rookTo = castleSquares.Value.rookTo; 
+                validMoves.Add(
+                    new CastleMove(Square, kingTo, rook.Square, rookTo)
+                );
+            }
+        }
+
+        return validMoves;
+    }
+
+
+    /// <summary>
+    /// Returns a tuple containing the squares of the king and rook after castling if they can castle. Otherwise, returns null.
+    /// </summary>
+    /// <param name="board"></param>
+    /// <param name="rook"></param>
+    /// <returns></returns>
+    public ((int row, int col) kingTo, (int row, int col) rookTo)? GetCastleSquares(Board board, RookPiece rook)
+    {
+        if (rook.Color == Color && rook.CanCastle(board))
+        {
+            int kingDirection = Math.Sign(rook.Row - Row);
+
+            (int row, int col) kingTo = (Row, Col + 2 * kingDirection);
+            (int row, int col) rookTo = (Row, kingTo.col - kingDirection);
+
+            return (kingTo, rookTo);
+        }
+
+        return null;
     }
 
 
