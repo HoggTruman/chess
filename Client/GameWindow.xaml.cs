fb52684533e08b35d16a6b3b868d180d48894a5b
Truman Hogg <hoggtruman@gmail.com>
2024-09-04 17:04:36 +0100

    Client cleanup

--- Client/GameWindow.xaml.cs
+++ Client/GameWindow.xaml.cs
@@ -9,16 +9,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Client
+namespace Client;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class GameWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public GameWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
