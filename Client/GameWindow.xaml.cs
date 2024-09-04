06eaeb6353073fd8440a55b824afc2ed2164ab9a
Truman Hogg <hoggtruman@gmail.com>
2024-09-04 18:47:53 +0100

    Add Piece images to GameWindow

--- Client/GameWindow.xaml.cs
+++ Client/GameWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using GameLogic;
+using GameLogic.Interfaces;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,8 +18,57 @@ namespace Client;
 /// </summary>
 public partial class GameWindow : Window
 {
+    #region Fields
+
+    private readonly Image[,] pieceImages = new Image[Board.BoardSize, Board.BoardSize];
+
+    private readonly Board _board = new();
+
+    #endregion
+
+
+
+    #region Constructors
+
     public GameWindow()
     {
         InitializeComponent();
+        InitializeBoard();
+        _board.Initialize(); // pass in from GameManager later??
+        DrawPieces();
     }
+
+    #endregion
+
+
+
+    #region Private Methods
+
+    private void InitializeBoard()
+    {
+        for (int rowIndex = 0; rowIndex < Board.BoardSize; rowIndex++)
+        {
+            for (int colIndex = 0; colIndex < Board.BoardSize; colIndex++)
+            {
+                Image image = new();
+                pieceImages[rowIndex, colIndex] = image;
+                PieceGrid.Children.Add(image);
+            }
+        }
+    }
+
+
+    private void DrawPieces()
+    {
+        for (int rowIndex = 0; rowIndex < Board.BoardSize; rowIndex++)
+        {
+            for (int colIndex = 0; colIndex < Board.BoardSize; colIndex++)
+            {
+                IPiece? piece = _board.State[rowIndex, colIndex];
+                pieceImages[rowIndex, colIndex].Source = Images.GetImageSource(piece);
+            }
+        }
+    }
+
+    #endregion
 }
---
+++ Client/Images.cs
@@ -0,0 +1,83 @@
+﻿using GameLogic.Enums;
+using GameLogic.Interfaces;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Client;
+
+public static class Images
+{
+    #region Fields
+
+    private static readonly Dictionary<PieceType, ImageSource> whiteSources = new()
+    {
+        [PieceType.Bishop] = LoadImage("Assets/WhiteBishop.png"),
+        [PieceType.King] = LoadImage("Assets/WhiteKing.png"),
+        [PieceType.Knight] = LoadImage("Assets/WhiteKnight.png"),
+        [PieceType.Pawn] = LoadImage("Assets/WhitePawn.png"),
+        [PieceType.Queen] = LoadImage("Assets/WhiteQueen.png"),
+        [PieceType.Rook] = LoadImage("Assets/WhiteRook.png"),
+    };
+
+    private static readonly Dictionary<PieceType, ImageSource> blackSources = new()
+    {
+        [PieceType.Bishop] = LoadImage("Assets/BlackBishop.png"),
+        [PieceType.King] = LoadImage("Assets/BlackKing.png"),
+        [PieceType.Knight] = LoadImage("Assets/BlackKnight.png"),
+        [PieceType.Pawn] = LoadImage("Assets/BlackPawn.png"),
+        [PieceType.Queen] = LoadImage("Assets/BlackQueen.png"),
+        [PieceType.Rook] = LoadImage("Assets/BlackRook.png"),
+    };
+
+    #endregion
+
+
+
+    #region Public Methods
+
+    /// <summary>
+    /// Provides the ImageSource that matches the given pieceType and color.
+    /// </summary>
+    /// <param name="pieceType">The type of piece</param>
+    /// <param name="color">The color of the piece</param>
+    /// <returns>The ImageSource correspond to the requested piece</returns>
+    public static ImageSource GetImageSource(PieceType pieceType, GameLogic.Enums.Color color)
+    {
+        return color switch
+        {
+            GameLogic.Enums.Color.White => whiteSources[pieceType],
+            GameLogic.Enums.Color.Black => blackSources[pieceType],
+            _ => null!
+        };
+    }
+
+
+    /// <summary>
+    /// Provides the ImageSource that matches the given piece.
+    /// </summary>
+    /// <param name="piece">An Ipiece</param>
+    /// <returns>If a piece is provided, the matching ImageSource. Otherwise, null.</returns>
+    public static ImageSource? GetImageSource(IPiece? piece)
+    {
+        if (piece == null)
+        {
+            return null;
+        }
+
+        return GetImageSource(piece.PieceType, piece.Color);
+    }
+
+    #endregion
+
+
+
+    #region Private Methods
+
+    private static ImageSource LoadImage(string filepath)
+    {
+        return new BitmapImage(new Uri(filepath, UriKind.Relative));
+    }
+
+    #endregion
+}
+
