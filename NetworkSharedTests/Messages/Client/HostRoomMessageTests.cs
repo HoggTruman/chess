64289437e73588f5e38147694e39ad54a813b304
Truman Hogg <hoggtruman@gmail.com>
2024-09-30 21:05:11 +0100

    Add message tests

---
+++ NetworkSharedTests/Messages/Client/HostRoomMessageTests.cs
@@ -0,0 +1,21 @@
+﻿using GameLogic.Enums;
+using NetworkShared.Messages.Server;
+
+namespace NetworkSharedTests.Messages.Client;
+
+public class HostRoomMessageTests
+{
+    [Theory]
+    [InlineData(PieceColor.White)]
+    [InlineData(PieceColor.Black)]
+    public void Encode_Decode_ReturnsOriginal(PieceColor hostColor)
+    {
+        // Arrange + Act
+        var encoded = HostRoomMessage.Encode(hostColor);
+        var decoded = HostRoomMessage.Decode(encoded);
+
+        // Assert
+        Assert.Equal(hostColor, decoded);
+    }
+}
+
---
+++ NetworkSharedTests/Messages/Server/RoomHostedMessageTests.cs
@@ -0,0 +1,21 @@
+﻿using GameLogic.Enums;
+using NetworkShared.Messages.Server;
+
+namespace NetworkSharedTests.Messages.Server;
+
+public class RoomHostedMessageTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(532)]
+    public void Encode_Decode_ReturnsOriginal(int roomId)
+    {
+        // Arrange + Act
+        var encoded = RoomHostedMessage.Encode(roomId);
+        var decoded = RoomHostedMessage.Decode(encoded);
+
+        // Assert
+        Assert.Equal(roomId, decoded);
+    }
+}
+
