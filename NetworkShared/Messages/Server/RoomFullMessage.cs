87353483d6f795d45483132b762431f6f344a96d
Truman Hogg <hoggtruman@gmail.com>
2024-10-05 18:10:31 +0100

    Add RoomFullMessage

---
+++ NetworkShared/Messages/Server/RoomFullMessage.cs
@@ -0,0 +1,26 @@
+﻿using NetworkShared.Enums;
+
+namespace NetworkShared.Messages.Server;
+
+public class RoomFullMessage
+{
+    // Encoded Message Structure:
+    // Byte 0: Message code
+    
+
+    public static ServerMessage Code { get; } = ServerMessage.RoomFull;
+
+
+    public static ServerMessage Decode()
+    {
+        return Code;
+    }
+
+
+    public static byte[] Encode()
+    {
+        byte codeByte = (byte)Code;
+        return [codeByte];
+    }
+}
+
---
+++ NetworkSharedTests/Messages/Server/RoomFullMessageTests.cs
@@ -0,0 +1,30 @@
+﻿using NetworkShared.Enums;
+using NetworkShared.Messages.Server;
+
+namespace NetworkSharedTests.Messages.Server;
+
+public class RoomFullMessageTests
+{
+    [Fact]
+    public void Decode_ReturnsCode()
+    {
+        // Arrange + Act
+        var decoded = RoomFullMessage.Decode();
+
+        // Assert
+        Assert.Equal(RoomFullMessage.Code, decoded);
+    }
+
+    [Fact]
+    public void Encode_ContainsCode()
+    {
+        // Arrange + Act
+        var encoded = RoomFullMessage.Encode();
+        var containedCode = (ServerMessage)encoded[0];
+
+        // Assert
+        Assert.Equal(RoomFullMessage.Code, containedCode);
+    }
+
+}
+
