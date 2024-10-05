44637627d07733dff333f3a9a220d1e6aaa0404a
Truman Hogg <hoggtruman@gmail.com>
2024-10-05 18:17:46 +0100

    Add RoomNotFoundMessage

---
+++ NetworkShared/Messages/Server/RoomNotFoundMessage.cs
@@ -0,0 +1,26 @@
+﻿using NetworkShared.Enums;
+
+namespace NetworkShared.Messages.Server;
+
+public class RoomNotFoundMessage
+{
+    // Encoded Message Structure:
+    // Byte 0: Message code
+    
+
+    public static ServerMessage Code { get; } = ServerMessage.RoomNotFound;
+
+
+    public static ServerMessage Decode()
+    {
+        return Code;
+    }
+
+
+    public static byte[] Encode()
+    {
+        byte codeByte = (byte)Code;
+        return [codeByte];
+    }
+}
+
---
+++ NetworkSharedTests/Messages/Server/RoomNotFoundMessageTests.cs
@@ -0,0 +1,30 @@
+﻿using NetworkShared.Enums;
+using NetworkShared.Messages.Server;
+
+namespace NetworkSharedTests.Messages.Server;
+
+public class RoomNotFoundMessageTests
+{
+    [Fact]
+    public void Decode_ReturnsCode()
+    {
+        // Arrange + Act
+        var decoded = RoomNotFoundMessage.Decode();
+
+        // Assert
+        Assert.Equal(RoomNotFoundMessage.Code, decoded);
+    }
+
+    [Fact]
+    public void Encode_ContainsCode()
+    {
+        // Arrange + Act
+        var encoded = RoomNotFoundMessage.Encode();
+        var containedCode = (ServerMessage)encoded[0];
+
+        // Assert
+        Assert.Equal(RoomNotFoundMessage.Code, containedCode);
+    }
+
+}
+
