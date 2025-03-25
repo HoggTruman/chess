2fdc3bd8106c2a30b3fd6cdba7fb13ec599edc89
Truman Hogg <hoggtruman@gmail.com>
2025-03-25 18:05:48 +0000

    Add Constructor

--- BetterGameLogic/Square.cs
+++ BetterGameLogic/Square.cs
@@ -2,6 +2,12 @@
 
 public readonly struct Square
 {
+    public Square(int row, int col)
+    {
+        Row = row;
+        Col = col;
+    }
+
     public int Row { get; init; }
     public int Col { get; init; }
 }
