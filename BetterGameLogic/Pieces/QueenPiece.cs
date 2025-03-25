609b1d93801477c3e0150c13037f1132d32fd30f
Truman Hogg <hoggtruman@gmail.com>
2025-03-25 21:31:39 +0000

    Add QueenPiece

---
+++ BetterGameLogic/Pieces/QueenPiece.cs
@@ -0,0 +1,44 @@
+using BetterGameLogic;
+using BetterGameLogic.Constants;
+using BetterGameLogic.Enums;
+using BetterGameLogic.Helpers;
+using BetterGameLogic.Pieces;
+
+namespace GameLogic.Pieces;
+
+public class QueenPiece : Piece
+{
+    public override PieceType PieceType => PieceType.Queen;
+    public override int Value => PieceValues.Queen;
+
+    public QueenPiece(Board board, int row, int col, PieceColor color)
+        : base(board, row, col, color)
+    {
+
+    }
+    
+
+    public override List<Square> GetTargetedSquares()
+    {
+        List<Square> targetedSquares = [];
+        targetedSquares.AddRange(PieceHelpers.GetTargetedRowColSquares(Row, Col, _board));
+        targetedSquares.AddRange(PieceHelpers.GetTargetedDiagonalSquares(Row, Col, _board));
+
+        return targetedSquares;
+    }
+
+
+    public override List<Square> GetReachableSquares()
+    {
+        List<Square> reachableSquares = [];
+        reachableSquares.AddRange(PieceHelpers.GetTargetedRowColSquares(Row, Col, _board));
+        reachableSquares.AddRange(PieceHelpers.GetTargetedDiagonalSquares(Row, Col, _board));
+
+        // Remove squares with a piece of the same color as the moving piece
+        reachableSquares = reachableSquares
+            .Where(s => _board.State[s.Row, s.Col]?.Color != Color)
+            .ToList();
+
+        return reachableSquares;
+    }
+}
