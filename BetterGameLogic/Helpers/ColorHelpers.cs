7e91b4bb81416904de7992d4879d4bb8c11e2a7d
Truman Hogg <hoggtruman@gmail.com>
2025-03-25 19:24:28 +0000

    Add ColorHelpers

---
+++ BetterGameLogic/Helpers/ColorHelpers.cs
@@ -0,0 +1,21 @@
+﻿using BetterGameLogic.Enums;
+
+namespace BetterGameLogic.Helpers;
+
+public static class ColorHelpers
+{
+    /// <summary>
+    /// Returns the opposite PieceColor
+    /// </summary>
+    /// <param name="color"></param>
+    /// <returns>A PieceColor</returns>
+    public static PieceColor Opposite(PieceColor color)
+    {
+        return color switch
+        {
+            PieceColor.White => PieceColor.Black,
+            PieceColor.Black => PieceColor.White,
+            _ => PieceColor.None
+        };
+    }
+}
