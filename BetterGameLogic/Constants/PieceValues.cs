d2a8dba07a29a95781c08f4d841b98016387ae1b
Truman Hogg <hoggtruman@gmail.com>
2025-03-25 18:09:47 +0000

    Add PieceValues

---
+++ BetterGameLogic/Constants/PieceValues.cs
@@ -0,0 +1,11 @@
+namespace BetterGameLogic.Constants;
+
+public static class PieceValues
+{
+    public const int Pawn = 1;
+    public const int Knight = 3;
+    public const int Bishop = 3;
+    public const int Rook = 5;
+    public const int Queen = 9;
+    public const int King = 0;
+}
