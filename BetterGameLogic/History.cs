480437edfbb01f291919a651574d68ad7c318c12
Truman Hogg <hoggtruman@gmail.com>
2025-03-28 20:06:27 +0000

    Add History support

---
+++ BetterGameLogic/History.cs
@@ -0,0 +1,21 @@
+﻿using BetterGameLogic.Moves;
+using BetterGameLogic.Pieces;
+
+namespace BetterGameLogic;
+
+public class History
+{
+    private readonly List<HistoryEntry> _entries = [];
+
+    public void AddEntry(IMove move, IPiece? capturedPiece)
+    {
+        _entries.Add(new(move, capturedPiece));
+    }
+
+    public HistoryEntry? LastOrDefault() => _entries.LastOrDefault();
+
+    public bool HasMoved(IPiece piece)
+    {
+        return _entries.Any(x => x.Move.MovesSquare(piece.StartSquare));
+    }
+}
---
+++ BetterGameLogic/HistoryEntry.cs
@@ -0,0 +1,10 @@
+﻿using BetterGameLogic.Moves;
+using BetterGameLogic.Pieces;
+
+namespace BetterGameLogic;
+
+public class HistoryEntry(IMove move, IPiece? capturedPiece)
+{
+    public readonly IMove Move = move;
+    public readonly IPiece? CapturedPiece = capturedPiece;
+}
