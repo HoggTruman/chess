5dcbfc04ecdf45d331f9d4fbc0ebdcb7e9dbc2d6
Truman Hogg <hoggtruman@gmail.com>
2025-04-02 19:45:30 +0100

    Add RotateSquare180

---
+++ BetterGameLogic/Helpers/BoardHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace BetterGameLogic.Helpers;
+
+public class BoardHelpers
+{
+    /// <summary>
+    /// Returns the square rotated 180 degrees.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns>Square representing the rotated square.</returns>
+    public static Square RotateSquare180(Square s)
+    {
+        return new(
+            Board.BoardSize - s.Row - 1, 
+            Board.BoardSize - s.Col - 1);
+    }
+}
